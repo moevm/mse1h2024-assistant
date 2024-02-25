78c78516f6775a70dfae012c0a5683eb476487bd
Коля Кузнецов <n1ck111555111@gmail.com>
2024-02-25 21:37:47 +0300

    gg

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import router from '@/router'
+import store from "@/store";
+
+import axios from 'axios';
+
+export const instance = axios.create({
+    baseURL: 'http://localhost:5000',  // Установка базового URL
+});
+
+createApp(App).use(router).use(store).mount('#app')
