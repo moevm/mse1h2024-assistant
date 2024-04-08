acadca4db3810a6833b9b6da22c46bcfd4bc4712
lenanaidyonova <lenanaidyonova123@mail.ru>
2024-04-09 00:43:41 +0300

    new bot

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -1,12 +1,10 @@
-import io
 import json
 import sys
-import aiohttp
-import asyncio
+
 import telebot
 import requests
-from modules.logger import build_logger
 import configparser
+
 
 def read_config():
     config = configparser.ConfigParser()
@@ -17,10 +15,70 @@ def read_config():
 
     return bot_token, backend_url
 
-async def main(bot):
-    user_data = {}
 
-    logger = build_logger('my_logger')
+def send_text_to_backend(backend_url, course, subject, question):
+    payload = {'course': course, 'subject': subject, 'text': question}
+    print(payload)
+    try:
+        response = requests.post(backend_url, json=payload)
+        response_data = response.json()
+        print(response_data)
+        response_text = response_data["text"]
+        if response_text:
+            print("RESPONSE:", response_text)
+            return response_text
+        else:
+            print("No 'text' field in response:", response_data)
+            return None
+    except Exception as e:
+        print("An error occurred while sending text data to backend:", str(e))
+
+
+def handle_text_message(message, user_data, bot, backend_url):
+    user_state = user_data.get(message.from_user.id, {}).get('state')
+
+    if user_state == 'course':
+        if not message.text.isdigit():
+            bot.send_message(message.chat.id, "Курс должен быть числом. Попробуйте еще раз.")
+            return
+        user_data[message.from_user.id]['course'] = message.text
+        bot.send_message(message.chat.id, "Пожалуйста, введите предмет.")
+        user_data[message.from_user.id]['state'] = 'subject'
+    elif user_state == 'subject':
+        user_data[message.from_user.id]['subject'] = message.text
+        bot.send_message(message.chat.id, "Задайте ваш вопрос.")
+        user_data[message.from_user.id]['state'] = 'question'
+    elif user_state == 'question':
+        course = user_data[message.from_user.id]['course']
+        subject = user_data[message.from_user.id]['subject']
+        question = message.text
+        send_text_to_backend(backend_url, course, subject, question)
+        bot.reply_to(message, "Твое сообщение обрабатывается.")
+        user_data[message.from_user.id] = {'state': 'course'}
+
+
+def handle_voice_message(message, user_data, bot, backend_url):
+    if 'course' not in user_data[message.from_user.id] or 'subject' not in user_data[message.from_user.id]:
+        bot.reply_to(message, "Для начала введите курс и предмет, используя команду \start")
+    else:
+        file_id = message.voice.file_id
+        file_info = bot.get_file(file_id)
+        voice_file = bot.download_file(file_info.file_path)
+        voice_string = voice_file.decode('latin-1')
+        bot.reply_to(message, "Твое голосовое сообщение обрабатывается.")
+
+        course = user_data[message.from_user.id]['course']
+        subject = user_data[message.from_user.id]['subject']
+        send_text_to_backend(backend_url, course, subject, voice_string)
+
+        user_data[message.from_user.id] = {'state': 'course'}
+
+
+def main():
+    bot_token, backend_url = read_config()
+    bot = telebot.TeleBot(bot_token)
+
+    user_data = {}
 
     @bot.message_handler(commands=['start'])
     def start(message):
@@ -28,85 +86,20 @@ async def main(bot):
                          "Добро пожаловать!\nБот не несет ответственности за представленную информацию\nВведите свой курс.")
         user_data[message.from_user.id] = {'state': 'course'}
 
-    def handle_text_message(message):
-        user_state = user_data.get(message.from_user.id, {}).get('state')
-
-        if user_state == 'course':
-            if not message.text.isdigit():
-                bot.send_message(message.chat.id, "Курс должен быть числом. Попробуйте еще раз.")
-                return
-            user_data[message.from_user.id]['course'] = message.text
-            bot.send_message(message.chat.id, "Пожалуйста, введите предмет.")
-            user_data[message.from_user.id]['state'] = 'subject'
-        elif user_state == 'subject':
-            user_data[message.from_user.id]['subject'] = message.text
-            bot.send_message(message.chat.id, "Задайте ваш вопрос.")
-            user_data[message.from_user.id]['state'] = 'question'
-        elif user_state == 'question':
-            course = user_data[message.from_user.id]['course']
-            subject = user_data[message.from_user.id]['subject']
-            question = message.text
-            asyncio.run_coroutine_threadsafe(send_text_to_backend(course, subject, question), loop)
-            bot.reply_to(message, "Твое сообщение обрабатывается.")
-            user_data[message.from_user.id] = {'state': 'course'}
-
-    def handle_voice_message(message):
-        if 'course' not in user_data[message.from_user.id] or 'subject' not in user_data[message.from_user.id]:
-            bot.reply_to(message, "Для начала введите курс и предмет, используя команду \start")
-        else:
-            file_id = message.voice.file_id
-            file_info = bot.get_file(file_id)
-            voice_file = bot.download_file(file_info.file_path)
-            voice_blob = io.BytesIO(voice_file)
-            voice_blob.seek(0)
-            voice_data = voice_blob.read()
-            voice_string = voice_data.decode('latin-1')
-            bot.reply_to(message, "Твое голосовое сообщение обрабатывается.")
-
-            course = user_data[message.from_user.id]['course']
-            subject = user_data[message.from_user.id]['subject']
-            asyncio.run_coroutine_threadsafe(send_text_to_backend(course, subject, voice_string), loop)
-
-            user_data[message.from_user.id] = {'state': 'course'}
-
-    async def send_text_to_backend(course, subject, question):
-        payload = json.dumps({'course': int(course), 'subject': subject, 'text': question})
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.post(backend_url, json=payload) as response:
-                    response_data = await response.json()
-                    response_text = response_data.get("text")
-                    if response_text:
-                        print("RESPONSE:", response_text)
-                        return response_text
-                    else:
-                        print("No 'text' field in response:", response_data)
-                        return None
-        except aiohttp.ClientError as err:
-            logger(f"Error sending text data to backend: {err}")
-            return None
-        except Exception as e:
-            logger(f"An error occurred while sending text data to backend: {e}")
-            return None
-
     @bot.message_handler(content_types=['voice'])
     def process_voice_message(message):
-        handle_voice_message(message)
+        handle_voice_message(message, user_data, bot, backend_url)
 
     @bot.message_handler(func=lambda message: True)
     def process_text_message(message):
-        handle_text_message(message)
-
-if __name__ == '__main__':
-    bot_token, backend_url = read_config()
-    bot = telebot.TeleBot(bot_token)
-    logger = build_logger('my_logger')
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(bot))
+        handle_text_message(message, user_data, bot, backend_url)
 
     try:
-        bot.infinity_polling(skip_pending=True)
+        bot.polling()
     except Exception as e:
-        logger.error("An error occurred while running the bot: %s", str(e))
-        bot.send_message("Произошла ошибка сервера")
+        print("An error occurred while running the bot:", str(e))
         sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
