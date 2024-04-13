9ba456003ce32aadc2be0fffa96369e813b17297
Andrexus23 <andrewsamsunguser@gmail.com>
2024-04-14 02:19:50 +0300

    доработан скрипт

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+sudo docker compose up -d ollama;
+sudo docker exec -it ollama ollama run mistral;
+pip install docker;
+python3 setup.py;
+sudo docker compose up -d;
 
-sudo docker compose up -d --build;
-sudo docker exec -it ollama ollama pull mistral
---
+++ setup.py
@@ -0,0 +1,18 @@
+import docker
+import configparser
+
+CONFIG_PATH = './backend/backend/config.ini'
+
+client = docker.DockerClient()
+config = configparser.ConfigParser()
+container = client.containers.get('ollama')
+inner_ip: str = container.attrs['NetworkSettings']['Networks']['mse1h2024-assistant_default']['IPAddress']
+port_key: str = list(container.attrs['NetworkSettings']['Ports'].keys())[0]
+inner_port: str = container.attrs['NetworkSettings']['Ports'][port_key][0]['HostPort']
+model_url: str = 'http://{0}:{1}'.format(inner_ip, inner_port)
+
+config.read(CONFIG_PATH)
+config['model']['url'] = model_url
+
+with open(CONFIG_PATH, 'w') as configfile:
+    config.write(configfile)
