db278ee5e296b1267c9d7ab87ecce8ef3f94a91b
Andrexus23 <andrewsamsunguser@gmail.com>
2024-04-14 03:19:07 +0300

    фиксы

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 sudo docker compose up -d ollama;
-sudo docker exec -it ollama ollama run mistral;
+sudo docker exec -it ollama ollama pull mistral;
 pip install docker;
 python3 setup.py;
-sudo docker compose up -d;
+sudo docker compose up -d --build;
 
