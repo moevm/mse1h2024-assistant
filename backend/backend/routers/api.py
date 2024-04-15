5809e43ae7aeacb404c1425a38c8769196da1a8f
PtichkinSergey <sergey.ptichkin@gmail.com>
2024-04-15 18:35:50 +0300

    add api

--- backend/backend/routers/api.py
+++ backend/backend/routers/api.py
@@ -1,7 +1,8 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form, File, UploadFile
 from backend.models.request import TextRequest
 from backend.models.client import OllamaClient
 from backend.settings import config
+import requests
 import os
 
 router = APIRouter(
@@ -22,5 +23,32 @@ def root():
 def ask_model_by_text(request: TextRequest):
     modelClient.readContextFromFile(os.path.join(dirname, '../../parser/new_data.json'), request.course, request.subject)
     answer = modelClient.sendPrompt(request.text)
+    return {'text': answer}
+
+@router.post("/send_voice_request")
+def handle_voice_request(course: int = Form(...), subject: str = Form(...), audio: UploadFile = File(...)):
+
+    url = "http://localhost:9000/asr"
+
+    payload = {
+        "input": {
+            "audio": audio,
+            "audio_base64": "None",
+            "transcription": "plain_text",
+            "translate": False,
+            "language": "ru",
+            "word_timestamps": False
+        },
+        "enable_vad": True
+    }
+    headers = {
+        "content-type": 'multipart/form-data'
+    }
+
+    transcription = requests.post(url, json=payload, headers=headers)
+
+    print("Transcript: ", transcription.text)
+    modelClient.readContextFromFile(os.path.join(dirname, '../../parser/new_data.json'), course, subject)
+    answer = modelClient.sendPrompt(transcription.text)
     return {'text': answer}
 
