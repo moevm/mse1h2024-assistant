2b0fa7bfd1c3a87c163ade1f3cb81591ba805bec
PtichkinSergey <sergey.ptichkin@gmail.com>
2024-04-15 18:28:39 +0300

    add request

--- frontend/src/requests.js
+++ frontend/src/requests.js
@@ -10,3 +10,18 @@ export async function post_request(course, subject, text){
       console.log("RESPONSE: " + response.data.text);
     return response.data.text;
 }
+
+export async function post_voice_request(course, subject, formData){
+  formData.append("course", course)
+  formData.append("subject", subject)
+  let response = await instance.post('/api/send_voice_request',
+      formData,
+      {
+          headers: {
+              'Content-Type': 'multipart/form-data'
+          }
+      }
+  )
+
+  return response.data.text;
+}
