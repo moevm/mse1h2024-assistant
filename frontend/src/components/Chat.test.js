7ced3ca26fa7bb7566d0a8aa17f76a02e46ef833
Кузнецов Николай <nakuznetsov1@stud.eltech.ru>
2024-04-23 02:00:32 +0300

    Chat base test

---
+++ frontend/src/components/Chat.test.js
@@ -0,0 +1,17 @@
+import { screen } from '@testing-library/vue';
+import Chat from '@/components/Chat.vue';
+import customRender from "@/customRender";
+import '@testing-library/jest-dom'
+
+
+
+describe('Chat', () => {
+    it('должен отображать начальное состояние компонента', () => {
+        // Рендерим компонент
+        customRender(Chat);
+
+        expect(screen.getByTestId('text-test')).toBeInTheDocument();
+        expect(screen.getByTestId('send-test')).toBeInTheDocument();
+        expect(screen.getByTestId('voice-test')).toBeInTheDocument();
+    });
+});
