8de32872c1735428d92ce4a535be2758532e0cdc
lenanaidyonova <lenanaidyonova123@mail.ru>
2024-05-19 19:10:07 +0300

    two tests

--- tg_bot/test_bot.py
+++ tg_bot/test_bot.py
@@ -1,64 +1,44 @@
-# test_integration.py
-
 import pytest
 from unittest.mock import Mock
-import time
+import configparser
 
-# Import functions to be tested
 from modules.handlers import handle_text_message, handle_voice_message
-from tg_bot.modules.logger import build_logger
+from modules.logger import build_logger
 
 
-# Fixture for setting up logger
 @pytest.fixture(scope="module")
 def logger():
     return build_logger('test_logger')
 
 
-# Fixture for generating mock user data
 @pytest.fixture(scope="module")
 def mock_user_data():
     return {}
 
 
-# Mock backend URL
-BACKEND_URL = "http://example.com"
+config = configparser.ConfigParser()
+config.read('config.ini')
+BACKEND_URL = config.get('Backend', 'url')
 
-# Mock bot object
 bot = Mock()
 
 
-# Test case for handling text message
 def test_handle_text_message(logger, mock_user_data):
     message = Mock()
-    message.from_user.id = 123456  # Mock user ID
-
-    # Set user state
+    message.from_user.id = 123456
     mock_user_data[message.from_user.id] = {'state': 'course'}
-
-    # Mock user input
-    message.text = "5"  # Mock course
-
-    # Call the function to be tested
+    message.text = "5"
     handle_text_message(message, mock_user_data, bot, BACKEND_URL, logger)
-
-    # Check if the state has been updated
     assert mock_user_data[message.from_user.id]['state'] == 'subject'
 
 
-# Test case for handling voice message
 def test_handle_voice_message(logger, mock_user_data):
     message = Mock()
-    message.from_user.id = 123456  # Mock user ID
-
-    # Set user state and data
+    message.from_user.id = 123456
     mock_user_data[message.from_user.id] = {'state': 'course', 'course': '5', 'subject': 'Math'}
-
-    # Mock user input
-    message.voice.file_id = "voice_file_id"  # Mock file ID
-
-    # Call the function to be tested
+    voice = Mock()
+    voice.file_id = "voice_file_id"
+    voice.duration = 15
+    message.voice = voice
     handle_voice_message(message, mock_user_data, bot, BACKEND_URL, logger)
-
-    # Check if the state has been updated
     assert mock_user_data[message.from_user.id]['state'] == 'course'
