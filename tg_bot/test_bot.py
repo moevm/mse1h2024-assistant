c9a51cdb1e6d560ac1de860446b336fe805717df
lenanaidyonova <lenanaidyonova123@mail.ru>
2024-05-19 20:19:23 +0300

    fix tests

--- tg_bot/test_bot.py
+++ tg_bot/test_bot.py
@@ -1,8 +1,7 @@
 import pytest
-import requests
 import configparser
-
-from modules.handlers import send_text_to_backend, send_voice_to_backend, get_task_result
+from unittest.mock import MagicMock
+from modules.handlers import send_text_to_backend, send_voice_to_backend, handle_voice_message, handle_text_message
 from modules.logger import build_logger
 
 
@@ -13,34 +12,23 @@ def backend_url():
     return config.get('Backend', 'url')
 
 
-# def test_send_text_to_backend(backend_url):
-#     course = '1'
-#     subject = 'Информатика'
-#     question = 'Рейтинг'
-#     logger = build_logger('test_logger')
-#
-#     response = send_text_to_backend(backend_url, course, subject, question, logger)
-#
-#     assert response is not None
-#
-#
-# def test_send_voice_to_backend(backend_url):
-#     course = '1'
-#     subject = 'Информатика'
-#     audio_blob = b'audio_data'
-#     logger = build_logger('test_logger')
-#
-#     response = send_voice_to_backend(backend_url, course, subject, audio_blob, logger)
-#
-#     assert response is not None
-
-
-def test_send_text_to_backend_with_long_message(backend_url):
+def test_send_text_to_backend(backend_url):
     course = '1'
     subject = 'Информатика'
-    question = 'A' * 150
+    question = 'Рейтинг'
     logger = build_logger('test_logger')
 
     response = send_text_to_backend(backend_url, course, subject, question, logger)
 
-    assert response == "Длина сообщения не должна превышать 100 символов."
+    assert response is not None
+
+
+def test_send_voice_to_backend(backend_url):
+    course = '1'
+    subject = 'Информатика'
+    audio_blob = b'audio_data'
+    logger = build_logger('test_logger')
+
+    response = send_voice_to_backend(backend_url, course, subject, audio_blob, logger)
+
+    assert response is not None
