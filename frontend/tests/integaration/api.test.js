1edd136b6b4f5785543b424de631cb865f51f224
Кузнецов Николай <nakuznetsov1@stud.eltech.ru>
2024-05-19 19:17:28 +0300

    done test of sending message

--- frontend/tests/integaration/api.test.js
+++ frontend/tests/integaration/api.test.js
@@ -5,4 +5,16 @@ describe("API backend", () => {
         const response = await instance.get('/api/get_courses');
         expect(response.status).toBe(200);
     });
+
+    it('should post message', async () => {
+        const response = await instance.post("/api/ask_model_by_text_request",
+            {
+                course: "course",
+                subject: "subject",
+                text: "text"
+            });
+        expect(response.status).toBe(200);
+    });
+
+
 })
