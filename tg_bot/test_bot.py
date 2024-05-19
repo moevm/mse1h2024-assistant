5b1624c8aa38b552cf1f1183199b71f8ddc3507f
lenanaidyonova <lenanaidyonova123@mail.ru>
2024-05-19 22:03:01 +0300

    add two tests

--- tg_bot/test_bot.py
+++ tg_bot/test_bot.py
@@ -1,6 +1,7 @@
 import pytest
 import configparser
-from unittest.mock import MagicMock
+import wave
+import io
 from modules.handlers import send_text_to_backend, send_voice_to_backend, handle_voice_message, handle_text_message
 from modules.logger import build_logger
 
@@ -32,3 +33,41 @@ def test_send_voice_to_backend(backend_url):
     response = send_voice_to_backend(backend_url, course, subject, audio_blob, logger)
 
     assert response is not None
+
+
+def test_send_long_text_to_backend(backend_url):
+    course = '1'
+    subject = 'Информатика'
+    long_question = 'a' * 200
+    logger = build_logger('test_logger')
+
+    response = send_text_to_backend(backend_url, course, subject, long_question, logger)
+
+    assert response is not None
+    assert "Ваш запрос слишком длинный! Максимальная поддерживаемая длина" in response.text
+
+
+def test_send_long_audio_to_backend(backend_url):
+    course = '1'
+    subject = 'Информатика'
+    logger = build_logger('test_logger')
+
+    # Генерация тестового аудиофайла длиной более 20 секунд
+    sample_rate = 44100  # 44.1 kHz
+    duration = 21  # 21 seconds
+    num_samples = sample_rate * duration
+    audio_data = (b'\x00\x00' * num_samples)
+
+    audio_blob = io.BytesIO()
+    with wave.open(audio_blob, 'wb') as wf:
+        wf.setnchannels(1)
+        wf.setsampwidth(2)
+        wf.setframerate(sample_rate)
+        wf.writeframes(audio_data)
+
+    audio_blob.seek(0)
+
+    response = send_voice_to_backend(backend_url, course, subject, audio_blob, logger)
+
+    assert response is not None
+    assert "Ваше аудиосообщение слишком длинное! Пожалуйста, запишите его покороче" in response.text
