3f0df5acac3cd4a9dbc87a7679da134606e37a8d
Andrexus23 <andrewsamsunguser@gmail.com>
2024-05-21 08:53:48 +0300

    update

--- tg_bot/bot.py
+++ tg_bot/bot.py
@@ -3,7 +3,9 @@ import telebot
 import configparser
 from modules.handlers import handle_text_message, handle_voice_message
 from modules.logger import build_logger
+from telebot import apihelper
 
+apihelper.proxy = {'http':'socks5://localhost:6000'}
 
 def read_config():
     config = configparser.ConfigParser()
@@ -35,6 +37,7 @@ def process_voice_message(message):
 
 @bot.message_handler(func=lambda message: True)
 def process_text_message(message):
+    print(message)
     handle_text_message(message, user_data, bot, backend_url, logger)
 
 
