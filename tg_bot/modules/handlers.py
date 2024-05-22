2a83e6858afb5b375a753b09873c2e6620dbda82
lenanaidyonova <lenanaidyonova123@mail.ru>
2024-05-22 23:32:06 +0300

    new fix

--- tg_bot/modules/handlers.py
+++ tg_bot/modules/handlers.py
@@ -52,35 +52,48 @@ def get_task_result(task_id, backend_url, logger):
 
 
 def handle_text_message(message, user_data, bot, backend_url, logger):
-    user_state = user_data.get(message.from_user.id, {}).get('state')
+    user_id = message.from_user.id
+    user_state = user_data.get(user_id, {}).get('state')
+
+    if user_data.get(user_id, {}).get('processing'):
+        bot.send_message(message.chat.id, "Ваш запрос ещё обрабатывается. Пожалуйста, подождите.")
+        return
 
     if user_state == 'course':
         if not message.text.isdigit():
             bot.send_message(message.chat.id, "Курс должен быть числом. Попробуйте еще раз.")
             return
-        user_data[message.from_user.id]['course'] = message.text
+        user_data[user_id]['course'] = message.text
         bot.send_message(message.chat.id, "Пожалуйста, введите предмет.")
-        user_data[message.from_user.id]['state'] = 'subject'
+        user_data[user_id]['state'] = 'subject'
     elif user_state == 'subject':
-        user_data[message.from_user.id]['subject'] = message.text
+        user_data[user_id]['subject'] = message.text
         bot.send_message(message.chat.id, "Задайте ваш вопрос.")
-        user_data[message.from_user.id]['state'] = 'question'
+        user_data[user_id]['state'] = 'question'
     elif user_state == 'question':
-        course = user_data[message.from_user.id]['course']
-        subject = user_data[message.from_user.id]['subject']
+        course = user_data[user_id]['course']
+        subject = user_data[user_id]['subject']
         question = message.text
         bot.reply_to(message, "Твое сообщение обрабатывается.")
+        user_data[user_id]['processing'] = True
         response_text = send_text_to_backend(backend_url, course, subject, question, logger)
         if response_text:
             bot.send_message(message.chat.id, response_text)
         else:
             bot.send_message(message.chat.id, "При получении ответа произошла ошибка.")
         bot.send_message(message.chat.id, "Введите ваш курс.")
-        user_data[message.from_user.id]['state'] = 'course'
+        user_data[user_id]['state'] = 'course'
+        user_data[user_id]['processing'] = False
 
 
 def handle_voice_message(message, user_data, bot, backend_url, logger):
-    if 'course' not in user_data[message.from_user.id] or 'subject' not in user_data[message.from_user.id]:
+    user_id = message.from_user.id
+
+    if user_data.get(user_id, {}).get('processing'):
+        bot.send_message(message.chat.id, "Ваш запрос ещё обрабатывается. Пожалуйста, подождите.")
+        return
+
+    if 'course' not in user_data[user_id] or 'subject' not in user_data[user_id]:
         bot.reply_to(message, "Для начала введите курс и предмет, используя команду \start.")
     else:
         if message.voice.duration > 12:
@@ -93,9 +106,10 @@ def handle_voice_message(message, user_data, bot, backend_url, logger):
         voice_file = bot.download_file(file_info.file_path)
         voice_string = voice_file.decode('latin-1')
         bot.reply_to(message, "Твое голосовое сообщение обрабатывается.")
+        user_data[user_id]['processing'] = True
 
-        course = user_data[message.from_user.id]['course']
-        subject = user_data[message.from_user.id]['subject']
+        course = user_data[user_id]['course']
+        subject = user_data[user_id]['subject']
         response_text = send_voice_to_backend(backend_url, course, subject, voice_string, logger)
 
         if response_text:
@@ -104,4 +118,5 @@ def handle_voice_message(message, user_data, bot, backend_url, logger):
             bot.send_message(message.chat.id, "При получении ответа произошла ошибка.")
 
         bot.send_message(message.chat.id, "Введите ваш курс.")
-        user_data[message.from_user.id]['state'] = 'course'
+        user_data[user_id]['state'] = 'course'
+        user_data[user_id]['processing'] = False
